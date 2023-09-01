9982981784dfc3867dbdccff2103eba72d7310ba
prasad <prasadkanna546@gmail.com>
2023-09-01 19:29:05 +0530

    fo

--- src/main/java/com/demoqa1/pages/TC_0001_TextBox_Page.java
+++ src/main/java/com/demoqa1/pages/TC_0001_TextBox_Page.java
@@ -9,5 +9,16 @@ public class TC_0001_TextBox_Page extends BaseClass {
 		
 		clickJS("clicktextbox_xpath");
 	}
+	
+	public void fil_TextBox_form(String fname, String email, String cadd,
+			String padd) throws Exception {
+		
+		getElement("fullname_xpath").sendKeys(fname);
+		getElement("email").sendKeys(email);
+		getElement("caddress_xpath").sendKeys(cadd);
+		getElement("paddress_xpath").sendKeys(padd);
+		Thread.sleep(2000);
+		clickJS("submit_xpath");
+	}
 
 }
--- src/test/java/com/demoqa1/testcases/TC_0001_TextBox_Test.java
+++ src/test/java/com/demoqa1/testcases/TC_0001_TextBox_Test.java
@@ -1,16 +1,28 @@
 package com.demoqa1.testcases;
 
+import java.util.Hashtable;
+
 import org.testng.annotations.Test;
 
 import com.demoqa1.pages.TC_0001_TextBox_Page;
+import com.demoqa1.utilities.Utility;
 
 public class TC_0001_TextBox_Test {
 	
-	@Test(groups = {"smoke","regression","sanity","functional"})
+	@Test(priority = 1, groups = {"smoke","regression","sanity","functional"})
 	public void click_TextBox() {
 		
 		TC_0001_TextBox_Page tb = new TC_0001_TextBox_Page();
 		tb.click_TextBox();
+		
+	}
+	
+	@Test(priority = 2, dataProviderClass=Utility.class, dataProvider = "demoQAdata", groups = {"smoke","regression","sanity","functional"})
+	public void fil_TextBox_form(Hashtable<String, String> data) throws Exception {
+		
+		TC_0001_TextBox_Page tb = new TC_0001_TextBox_Page();
+		tb.fil_TextBox_form(data.get("fname"), data.get("email"),
+				data.get("cadd"), data.get("padd"));
 		
 	}
 
